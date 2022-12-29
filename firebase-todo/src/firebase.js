436ed2eda24b81bf02ca0a31c3a87db6c79adb0d
Roger <90182060+rogerUserGitHub@users.noreply.github.com>
2022-12-29 16:31:23 +0100

    update config

--- firebase-todo/src/firebase.js
+++ firebase-todo/src/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArwjNz4KeBRnKlfXOvhcGtIqVSlOwLiZE",
-  authDomain: "todo-app-fedc3.firebaseapp.com",
-  projectId: "todo-app-fedc3",
-  storageBucket: "todo-app-fedc3.appspot.com",
-  messagingSenderId: "516763189134",
-  appId: "1:516763189134:web:90bbc074febfc508026edf"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: 'todo-app-fedc3.firebaseapp.com',
+  projectId: 'todo-app-fedc3',
+  storageBucket: 'todo-app-fedc3.appspot.com',
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
