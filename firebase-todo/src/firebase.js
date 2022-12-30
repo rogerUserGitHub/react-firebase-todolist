b8cfbf45b20774f8eb6e918871d3706803a9a613
Roger <90182060+rogerUserGitHub@users.noreply.github.com>
2022-12-30 10:16:31 +0100

    update config

--- firebase-todo/src/firebase.js
+++ firebase-todo/src/firebase.js
@@ -7,9 +7,9 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: 'todo-app-fedc3.firebaseapp.com',
-  projectId: 'todo-app-fedc3',
-  storageBucket: 'todo-app-fedc3.appspot.com',
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
